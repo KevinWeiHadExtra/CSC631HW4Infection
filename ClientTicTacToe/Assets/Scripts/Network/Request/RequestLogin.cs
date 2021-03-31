cb3a551e710af66db639dac65c5b0422a9cdb22a
Zach <60633052+Area-Turtle@users.noreply.github.com>
2021-03-30 22:31:53 -0700

    bug fixes

---
+++ ClientTicTacToe/Assets/Scripts/Network/Request/RequestLogin.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RequestLogin : NetworkRequest
+{
+	public RequestLogin()
+	{
+		request_id = Constants.CMSG_LOGIN;
+	}
+
+	public void send()
+	{
+		packet = new GamePacket(request_id);
+	}
+}
